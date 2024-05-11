973503b728b68b588ba73cad6db0e5eb5de18dc8
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-05-11 23:39:56 +0300

    Delete misc.py

--- misc.py
+++
@@ -1,9 +0,0 @@
-import asyncio, sqlite3
-from config import BOT_TOKEN
-from aiogram import Bot, Dispatcher, types
-
-loop = asyncio.get_event_loop()
-bot = Bot(BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-dp = Dispatcher(bot=bot, loop=loop)
-conn = sqlite3.connect("db.db")
-cursor = conn.cursor()
