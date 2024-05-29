852fdb8e3ae50c594ccc122f1cb0ead2e276b3e0
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-05-29 22:47:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_BOT = '6542734064:AAGYEBv8My1nSOiJK3CY85J3U3qjP3gDIag'
 ADMINS = [6888643375, 403607590]
 
-TELEGRAM_CHANNEL = "https://t.me/geormor"
+TELEGRAM_CHANNEL = "https://t.me/geormor123"
 MENEDGER = "https://t.me/geormor"
 
 # ПРАЦЮЄ ВЖЕ РІВНО 2 години 20 хвилин
