e453e812d495e08c09dcb587c021234d0b4e8359
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-06-18 21:17:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ import commands.entertaining.rz
 import commands.basic.top
 import commands.clans.main
 import commands.admin.promo
+import commands.punishment
 
 
 @dp.message_handler(commands=['start'])
@@ -204,6 +205,7 @@ if __name__ == '__main__':
     commands.entertaining.earnings.tree.main.reg(dp)
     commands.basic.ore.main.reg(dp)
     commands.help.reg(dp)
+    commands.punishment.reg(dp)
     commands.entertaining.rz.reg(dp)
     commands.basic.top.reg(dp)
     commands.entertaining.wedlock.reg(dp)
