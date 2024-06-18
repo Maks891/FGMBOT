dc910061a4a2ed7078e818cf358fded8a056ceab
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-06-18 20:08:00 +0300

    Update main.py

--- commands/main.py
+++ commands/main.py
@@ -9,16 +9,16 @@ async def on_start(message):
     sticker_id = 'CAACAgQAAxkBAAEKs6JlSQUtGTtSzXGVcJGBe0PwnWkI9QACRwkAAm0NeFIe5FE9nk15XTME'
     await message.answer_sticker(sticker=sticker_id)
 
-    await message.answer(f'''🤖 Добро пожаловать на борт, Кто-то! Меня зовут BFG, твой верный игровой бот.
+    await message.answer(f'''🤖 Добро пожаловать на борт, Кто-то! Меня зовут FGM, твой верный игровой бот.
 
 🎮 У меня есть множество интересных команд и игр, чтобы скрасить твоё время, будь ты один или в компании друзей! (Кстати, вместе всегда веселее) 💙
 🔍 Познакомиться со всеми моими возможностями ты можешь, введя команду «помощь».
 
-<a href="{cfg.chanell}">🔈 Наш канал</a>
-<a href="{cfg.chat}">💬 Наш чат</a>''', disable_web_page_preview=True, reply_markup=kb.start())
+<a href="https://t.me/FGMINFO">🔈 Наш канал</a>
+<a href="https://t.me/FGMOFF">💬 Наш чат</a>''', disable_web_page_preview=True, reply_markup=kb.start())
 
     await message.answer(f'''🚀 Не уверен, с чего начать своё приключение?
-Присоединяйся к нашему официальному чату {cfg.bot_name}: {cfg.chat}''', disable_web_page_preview=True)
+Присоединяйся к нашему официальному чату FGM: https://t.me/FGMOFF''', disable_web_page_preview=True)
 
 
 async def win_luser():
@@ -28,6 +28,13 @@ async def win_luser():
     rloser = random.choice(loser)
     return rwin, rloser
 
+
+async def yznat_cmd(message: types.Message):
+    if message.reply_to_message:
+        mentioned_user_id = message.reply_to_message.from_user.id
+        await message.answer(f"ID человека: {mentioned_user_id}")
+    else:
+        await message.answer("Сообщение пользователя не было отмечено.")
 
 async def geturl(id, txt):
     url = f'<a href="tg://user?id={id}">{txt}</a>'
