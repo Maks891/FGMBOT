4ce06cc5c17a616d22ee715adb8520a18cc24fe9
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-10-09 16:24:14 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+API_TOKEN = '7092521991:AAG18Ty2cie-czUSvfhQmyWo9sZmIpHtYos'
+
+admin = [6888643375]
+start_money = 10000
+
+bot_name = 'BFG'
+chat = 't.me/FGMOFICIALL'
+chanell = 't.me/FGMCDF'
+admin_username = '@geormor'
+bot_username = 'FGM'
+
+chat_log = 0
+cleaning = 60
