322bbecba20fc06ad58973dc91942893056ecc28
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-10-10 13:50:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ admin = [6888643375, 5244814453]
 start_money = 10000
 
 bot_name = 'FGM'
-chat = 'https://t.me/FGMOFICIAL'
-chanell = 'https://t.me/FGMINFO'
+chat = 't.me/FGMOFICIAL'
+chanell = 't.me/FGMINFO'
 admin_username = '@geormor'
 bot_username = 'FGM'
 
